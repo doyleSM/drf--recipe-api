68513966f0301f598b5e6154abcff8f81648aa49
Gabriel Doyle Balk <gdoyle.dev@gmail.com>
2020-07-26 14:45:51 -0300

    add manageuser endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -20,6 +20,16 @@ class UserSerializer(serializers.ModelSerializer):
         """create a new user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """update a user setting the password correctly"""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """serializer for the user authentication object"""
