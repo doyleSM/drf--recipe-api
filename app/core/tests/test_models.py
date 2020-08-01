7c909e2aaf4940769bed4aca7abdd577674f5af6
Gabriel Doyle Balk <gdoyle.dev@gmail.com>
2020-08-01 14:54:39 -0300

    add create tag

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email="teste@gmail.com", password="testepass"):
+    """"create a sample_user"""
+    return get_user_model().objects.create(email=email, password=password)
 
 
 class ModelTests(TestCase):
@@ -36,3 +42,12 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """test the tag string representional"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name="Barbecue"
+        )
+
+        self.assertEqual(str(tag), tag.name)
