fa28790902c66551d5739d3d302c9538b6ceab78
Gabriel Doyle Balk <gdoyle.dev@gmail.com>
2020-08-02 19:46:43 -0300

    add image

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -45,3 +45,11 @@ class RecipeDetailSerializer(RecipeSerializer):
     """serializer for recipe detail"""
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """serializer for uploading image to recipes"""
+    class Meta:
+        model = Recipe
+        fields = ('id', 'image')
+        read_only_fields = ('id',)
